be83686be96d2746a3fef1811dc79ea3a6762eb7
Jesse Zelaya <jessezelaya1@gmail.com>
2015-10-31 13:56:28 -0700

    Added blog route to serve mock blog posts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var posts   = require('./mock/posts.json');
 var app     = express();
 
 app.get('/', function(req,res){
-  res.send('Loaded Root Page')
-})
+  res.send('Loaded Root Page');
+});
 
+
+app.get('/blog', function(req,res){
+  res.send(posts)
+})
 
 app.listen(3000, function(){
   console.log('Listening on Port 3000!');
-})
+});
