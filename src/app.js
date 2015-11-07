6b1963d487978274eec81ab1c92f6586ca3868d5
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-07 12:19:43 -0800

    Set up to serve static files on front end

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ var express = require('express');
 var posts   = require('./mock/posts.json');
 var app     = express();
 
+
+//Serve public assests
+app.use('/static', express.static(__dirname + '/public'));
+
 //Set view engine to Jade
 app.set('view engine', 'jade');
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views');
 
 //Home Route
 app.get('/', function(req,res){
@@ -15,11 +19,11 @@ app.get('/', function(req,res){
 app.get('/blog/:title?', function(req,res){
   var title = req.params.title;
   if (title === undefined) {
-    res.status(503)
+    res.status(503);
     res.send('This page is under construction');
   }else {
     var post  = posts[title] || {};
-    res.render('post', {post: post})
+    res.render('post', {post: post});
   }
 });
 
